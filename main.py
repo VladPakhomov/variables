cd5038991de108c14142cdac2302768d810883c0
VladPakhomov <mongebello@gmail.com>
2024-12-03 00:08:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+the_number_of_completed = 12
+hours = 1.5
+course_name = 'python'
+print (1.5/ 12)
+one_task = 0.125
+print(course_name,'курс','всего задач',the_number_of_completed,'затрачено часов',hours,'среднее время выполнения',one_task,'часов')
